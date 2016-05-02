45348cddad505810aba80c2e5e02d31464d42ee9
nicolasiscoding <nicolascoding@gmail.com>
2016-05-02 17:37:01 -0400

    pushing git ignore fix

--- config/database.js
+++
@@ -1,6 +0,0 @@
-// config/database.js
-module.exports = {
-
-	'url' : 'mongodb://archit:uflacm@ds017231.mlab.com:17231/uflacm' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
-};
