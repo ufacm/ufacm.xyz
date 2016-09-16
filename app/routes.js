baf54e5e9739e54d6e366f4db8e337f3a4839091
progdude <architrathi10@gmail.com>
2016-09-16 13:08:48 -0400

    will comeback to it

--- app/routes.js
+++ app/routes.js
@@ -9,6 +9,7 @@ const auth = require('http-auth');
 const formidable = require('formidable');
 const cpy = require('cpy');
 const fsaccess = require('fs-access');
+require('express-zip');
 
 // const util = require('util');
 
@@ -26,13 +27,13 @@ const secEvents = require('./middleware/secEvents');
 module.exports = (app, passport, mongoose) => {
 
     // setup resume repo password for Companies Only
-    const basic = auth.basic({
-        realm: 'Sponsoring companies only.',
-        file: __dirname + '/users.htpasswd'
-      });
+    // const basic = auth.basic({
+    //     realm: 'Sponsoring companies only.',
+    //     file: __dirname + '/users.htpasswd'
+    //   });
 
     // get the page for all student repo's
-    app.get('/repo', auth.connect(basic), (req, res) => {
+    app.get('/repo', (req, res) => {
         res.render('resume/repo.ejs', {
             user: req.user
           });
@@ -54,27 +55,26 @@ module.exports = (app, passport, mongoose) => {
           });
       });
 
-    // app.post('/testpdf', (req, res) => {
-    //     let pdfs = [];
-    //
-    //     for (let i = 0; i < req.body.pdfs.length; i++) {
-    //       let str = req.body.pdfs[i].slice(24, req.body.pdfs[i].length);
-    //       pdfs.push({
-    //               path: 'localStorage/pdfs/' + req.body.pdfs[i],
-    //               name: str
-    //             });
-    //
-    //       // pdfs.push(req.body.pdfs[i]);
-    //     }
-    //
-    //     console.log(pdfs);
-    //     res.send('asdf');
-    //   });
+    let pdfs = [];
+    app.post('/testpdf', (req, res) => {
+
+        for (let i = 0; i < req.body.pdfs.length; i++) {
+          let str = req.body.pdfs[i].slice(24, req.body.pdfs[i].length);
+          pdfs.push({
+                  path: __dirname + '/../localStorage/pdfs/' + req.body.pdfs[i],
+                  name: str
+                });
+
+          // Assuming localstorage/pdfs is name on local directory
+        }
+
+        res.send('');
+      });
 
     // mass download PDFs but this is still not secure
-    // app.get('/downloadPDF', (req, res) => {
-    //     res.zip(pdfs);
-    //   });
+    app.get('/downloadPDF', (req, res) => {
+        res.zip(pdfs);
+      });
 
     app.get('/feed', (req, res) => {
         res.render('feed.ejs');
@@ -247,7 +247,7 @@ module.exports = (app, passport, mongoose) => {
       });
 
     app.get('/getStudentResumes', (req, res) => {
-        if (Object.keys(req.body)[0] === null) {
+        if (Object.keys(req.body)[0] === undefined) {
           User.find({}, (err, users) => {
               res.send(users);
             });
--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'MongoDBURIExample' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://archit:gogators@ds031835.mlab.com:31835/uflacm' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
